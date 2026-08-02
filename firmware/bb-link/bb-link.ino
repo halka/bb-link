fe00e07eacbf69a4f07bbfff2130b05b7f651352
halka <halka@rjch.jp>
2026-08-02 18:27:12 +0900

    Revert "bb-link.ino の更新"
    This reverts commit 2de6329cc4426c1eefcb419e2e939d493f61b94b.

--- firmware/bb-link/bb-link.ino
+++ firmware/bb-link/bb-link.ino
@@ -1,6 +1,6 @@
 /*
-  (C) 2024 Island Magic Co. All Rights Reserved.
-  (C) 2025-2026 JM8UTW
+  (c) 2024 Island Magic Co. All Rights Reserved.
+
   M5 ATOM LITE port (M5Unified). External power assumed.
 
   This program is free software: you can redistribute it and/or modify
@@ -41,7 +41,6 @@ void setup() {
   Serial.println("| _ ) | _ )   | |  (_)_ _ | |__");
   Serial.println("| _ \\_| _ \\_  | |__| | ' \\| / /");
   Serial.println("|___(_)___(_) |____|_|_||_|_\\_\\\n");
-  Serial.println("JM8UTW MOD AUG26\n");
 
   Serial.printf("Booting up %s v%d.%d.%d on %s v%d.%d\n\n",
                 adapter->getAdapterName().c_str(),
